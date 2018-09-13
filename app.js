dc03e406273c9ef3a2837b5395a4bd764100c3ac
sangumee <super2451894@gmail.com>
2018-09-13 15:18:36 +0900

    Fix app.js
    FIx app.js to change method ajax
    
    This version of this project is works with ajax and works well

--- app.js
+++ app.js
@@ -9,49 +9,36 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
 
-        const unsplashRequest = new XMLHttpRequest();
-
-        unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        unsplashRequest.onload = addImage;
-        unsplashRequest.setRequestHeader('Authorization', 'Client-ID 89511a20975117a2d7d1c3fd1904517bb7326531502dc28a78dd73bb67269e4a');
-        unsplashRequest.send();
-
-        function addImage() {
-            let htmlContent = '';
-            const data = JSON.parse(this.responseText);
-
-            if (data && data.results && data.results[0]) {
-                const firstImage = data.results[0];
-                htmlContent = `<figure>
-                    <img src="${firstImage.urls.regular}" alt="${searchedForText}">
-                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
-                    </figure>
-                `;
-            } else {
-                htmlContent = '<div class="error-no-image">No images Available</div>';
+        // Use ajax Method to show unsplash Images
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                Authorization: 'Client-ID 89511a20975117a2d7d1c3fd1904517bb7326531502dc28a78dd73bb67269e4a'
             }
-            responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+        }).done(addImage);
+
+        function addImage(images) {
+            const firstImage = images.results[0];
+
+            responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+                    <img src="${firstImage.urls.small}" alt="${searchedForText}">
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                </figure>`);
         }
 
-        const articleRequest = new XMLHttpRequest();
-        articleRequest.onload = addArticles;
-        articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=84769d8f2a284e4eb474fa4f82311714`);
-        articleRequest.send();
+        // Use ajax Method to show NYT Articles
+        $.ajax({
+            url:`https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=84769d8f2a284e4eb474fa4f82311714`
+        }).done(addArticles);
 
-        function addArticles() {
+        function addArticles(articles) {
 
-            let htmlContent = '';
-            const data = JSON.parse(this.responseText);
+            const data= articles;
+            responseContainer.insertAdjacentHTML('beforeend', '<ul>' + data.response.docs.map(article => `<li class="article">
+            <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
+            <p>${article.snippet}</p>
+            </li>`).join('') + '</ul>')
 
-            if (data.response && data.response.docs && data.response.docs.length > 1) {
-                htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
-                    <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
-                    <p>${article.snippet}</p>
-                    </li>`).join('') + '</ul>';
-            } else {
-                htmlContent = '<div class="error-no-articles">No articles available</div>';
-            }
-            responseContainer.insertAdjacentHTML('beforeend', htmlContent);
         }
 
     });
